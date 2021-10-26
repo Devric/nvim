3fb2459c8ed4972f7f2c28f868c61f57df77a065
Billy Huang <billbox13@gmail.com>
2021-10-27 00:33:59 +1100

    Update tabout

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -61,7 +61,7 @@ require('packer').startup(function()
 
 	-- UTILITY
 	use 'AndrewRadev/splitjoin.vim'
- 	use 'editorconfig/editorconfig-vim'
+	use 'editorconfig/editorconfig-vim'
 	use {
 		"folke/which-key.nvim",
 		config = function() require("which-key").setup {
@@ -110,14 +110,33 @@ require('packer').startup(function()
 		end
 	}
 	use 'sindrets/diffview.nvim'
+
 	use {
 		'abecodes/tabout.nvim',
 		config = function()
 			require('tabout').setup {
 				tabkey = "",
 				backwards_tabkey = "",
+				-- tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
+				-- backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
+				act_as_tab = true, -- shift content if tab out is not possible
+				act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+				enable_backwards = true, -- well ...
+				completion = true, -- if the tabkey is used in a completion pum
+				tabouts = {
+					{open = "'", close = "'"},
+					{open = '"', close = '"'},
+					{open = '`', close = '`'},
+					{open = '(', close = ')'},
+					{open = '[', close = ']'},
+					{open = '{', close = '}'}
+				},
+				ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+				exclude = {} -- tabout will ignore these filetypes
 			}
-		end
+		end,
+		wants = {'nvim-treesitter'}, -- or require if not used so far
+		after = {'coq'} -- if a completion plugin is using tabs load it before
 	}
 	
 	-- allows quickly select window
